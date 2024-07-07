46262c0a3059488f86bda8a995bd3618dac14188
EbbaGronvall <ebbagronvall@hotmail.com>
2024-07-07 22:39:18 +0200

    Add more tests and functions

--- scripts/game.js
+++ scripts/game.js
@@ -5,5 +5,29 @@ let game = {
     score: 0,
 };
 
+function newGame() {
+    game.score = 0;
+    game.playerMoves = [];
+    game.currentGame = [];
+    showScore();
+    addTurn();
+};
 
-module.exports = { game };
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    //showTurns();
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add("light");
+    setTimeout(() => {
+        document.getElementById(circ).classList.remove("light");
+    }, 400);
+}
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn };
